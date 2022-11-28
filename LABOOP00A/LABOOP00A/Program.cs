a4c350b9a6489e74452fc40a6f96370806c471a2
นายชาญชล บุบปับพา 623410004-6 <tanapattara.w@gmail.com>
2022-11-28 16:31:34 +0700

    add sample

--- LABOOP00A/LABOOP00A/Program.cs
+++ LABOOP00A/LABOOP00A/Program.cs
@@ -1,3 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-// print string to commanline
-Console.WriteLine("Tanapattara Wongkhamchan");
+//Data type
+using LABOOP00A;
+
+Student student1 = new Student();
+student1.name = "Mickey";
+student1.weight = 20.8f;
+
+Student student2 = new Student();
+student2.name = "Minnie";
+student2.weight = 18.2f;
+
+float avgWeight = (student1.weight + student2.weight)/2;
+Console.WriteLine("Weight avg of Student:" + avgWeight);
---
+++ LABOOP00A/LABOOP00A/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LABOOP00A
+{
+    public class Student
+    {
+        public string name;
+        public float weight;
+    }
+}
